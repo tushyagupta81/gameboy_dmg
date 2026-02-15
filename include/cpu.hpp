b4d15ac46403cb8636f8069254a73a7db474c8dd
Tushya Gupta <tushyagupta81@gmail.com>
2026-02-16 00:06:16 +0530

    more instructions

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -25,13 +25,29 @@ private:
 
   auto nop() -> uint8_t;
 
-  auto ld_r_r() -> uint8_t;
-  auto ld_r_d8() -> uint8_t;
-  auto inc_r() -> uint8_t;
-  auto dec_r() -> uint8_t;
+  auto ld_r8_r8() -> uint8_t;
+  auto ld_r8_i8() -> uint8_t;
+  auto ld_mem_a() -> uint8_t;
+  auto ld_a_mem() -> uint8_t;
+  auto ld_i16_sp() -> uint8_t;
+  auto ld_r16_i16() -> uint8_t;
+
+  auto inc_r8() -> uint8_t;
+  auto inc_r16() -> uint8_t;
+
+  auto dec_r8() -> uint8_t;
+  auto dec_r16() -> uint8_t;
+
+  auto add_hl_r16() -> uint8_t;
+  auto add_a_r8() -> uint8_t;
+
+  auto adc_a_r8() -> uint8_t;
 
   void write_reg(uint8_t dst, uint8_t val);
   auto read_reg(uint8_t src) -> uint8_t;
+
+  void write_r16(uint8_t dst, uint16_t val);
+  auto read_r16(uint8_t src) -> uint16_t;
 
   std::array<Instr, 256> cb_table{}, op_table{};
 };
--- src/core/cpu.cpp
+++ src/core/cpu.cpp
@@ -1,4 +1,5 @@
 #include "cpu.hpp"
+#include "memory_map.hpp"
 #include <cstdint>
 
 auto CPU::step() -> uint8_t {
@@ -16,32 +17,143 @@ auto CPU::step() -> uint8_t {
 }
 
 void CPU::init_tables() {
+  // https://gbdev.io/pandocs/CPU_Instruction_Set.html
+  // BLOCK 0 -> first 5 tables done last 3 left
   op_table[0x00] = &CPU::nop;
 
+  for (uint8_t i = 0x1; i <= 0x31; i += 16) {
+    op_table[i] = &CPU::ld_r16_i16;
+  }
+
+  for (uint8_t i = 0x2; i <= 0x32; i += 16) {
+    op_table[i] = &CPU::ld_mem_a;
+  }
+
+  for (uint8_t i = 0x3; i <= 0x33; i += 16) {
+    op_table[i] = &CPU::inc_r16;
+  }
+
+  for (uint8_t i = 0x04; i <= 0x3C; i += 8) {
+    op_table[i] = &CPU::inc_r8;
+  }
+
+  for (uint8_t i = 0x05; i <= 0x3D; i += 8) {
+    op_table[i] = &CPU::dec_r8;
+  }
+
   for (uint8_t i = 0x6; i <= 0x3E; i += 8) {
-    op_table[i] = &CPU::ld_r_d8;
+    op_table[i] = &CPU::ld_r8_i8;
   }
 
-  for (uint8_t i = 0x04; i <= 0x3C; i += 0x08) {
-    op_table[i] = &CPU::inc_r;
+  op_table[0x08] = &CPU::ld_i16_sp;
+
+  for (uint8_t i = 0x9; i <= 0x39; i += 16) {
+    op_table[i] = &CPU::add_hl_r16;
   }
 
-  for (uint8_t i = 0x05; i <= 0x3D; i += 0x08) {
-    op_table[i] = &CPU::dec_r;
+  for (uint8_t i = 0xA; i <= 0x3A; i += 16) {
+    op_table[i] = &CPU::ld_a_mem;
   }
 
+  for (uint8_t i = 0xB; i <= 0x3B; i += 16) {
+    op_table[i] = &CPU::dec_r16;
+  }
+
+  // BLOCK 1 halt inst left
   for (uint8_t i = 0x40; i <= 0x4F; i++) {
     if (i == 0x76) {
       // op_table[i] =
       continue;
     }
-    op_table[i] = &CPU::ld_r_r;
+    op_table[i] = &CPU::ld_r8_r8;
+  }
+
+  // BLOCK 2
+  for (uint8_t i = 0x80; i <= 0x87; i++) {
+    op_table[i] = &CPU::add_a_r8;
+  }
+  for (uint8_t i = 0x88; i <= 0x8F; i++) {
+    op_table[i] = &CPU::adc_a_r8;
   }
 }
 
 auto CPU::nop() -> uint8_t { return 4; }
 
-auto CPU::ld_r_r() -> uint8_t {
+auto CPU::adc_a_r8() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t src = opcode & 0x07;
+
+  uint8_t val = read_reg(src);
+  uint8_t a = reg.a;
+  auto carry = static_cast<uint8_t>(reg.get_flag(gb::mem::FLAG_C));
+
+  uint16_t result = a + val + carry;
+  auto final = static_cast<uint8_t>(result);
+
+  reg.set_flag(gb::mem::FLAG_Z, final == 0);
+  reg.set_flag(gb::mem::FLAG_N, false);
+  reg.set_flag(gb::mem::FLAG_H, ((a & 0x0F) + (val & 0x0F) + carry) > 0x0F);
+  reg.set_flag(gb::mem::FLAG_C, result > 0xFF);
+
+  reg.a = final;
+
+  if (src == 6) {
+    return 8;
+  }
+  return 4;
+}
+
+auto CPU::add_a_r8() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t src = opcode & 0x07;
+
+  uint8_t val = read_reg(src);
+  uint8_t a = reg.a;
+
+  uint16_t result = a + val;
+  auto final = static_cast<uint8_t>(result);
+
+  reg.set_flag(gb::mem::FLAG_Z, final == 0);
+  reg.set_flag(gb::mem::FLAG_N, false);
+  reg.set_flag(gb::mem::FLAG_H, ((a & 0x0F) + (val & 0x0F)) > 0x0F);
+  reg.set_flag(gb::mem::FLAG_C, result > 0xFF);
+
+  reg.a = final;
+
+  if (src == 6) {
+    return 8;
+  }
+  return 4;
+}
+
+auto CPU::ld_r16_i16() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t dst_reg = (opcode >> 4) & 0x3;
+
+  uint8_t low = bus.read(reg.pc++);
+  uint8_t high = bus.read(reg.pc++);
+
+  uint16_t val = (high << 8) | low;
+
+  write_r16(dst_reg, val);
+
+  return 12;
+}
+
+auto CPU::ld_i16_sp() -> uint8_t {
+  uint8_t low_byte = bus.read(reg.pc++);
+  uint8_t high_byte = bus.read(reg.pc++);
+
+  uint16_t addr = (high_byte << 8) | low_byte;
+
+  // write SP little-endian
+  bus.write(addr, reg.sp & 0xFF);            // low byte
+  bus.write(addr + 1, (reg.sp >> 8) & 0xFF); // high byte
+
+  return 20;
+}
+
+auto CPU::ld_r8_r8() -> uint8_t {
   uint8_t opcode = bus.read(reg.pc - 1);
 
   uint8_t dst = (opcode >> 3) & 0x07;
@@ -55,7 +167,7 @@ auto CPU::ld_r_r() -> uint8_t {
   return 4;
 }
 
-auto CPU::ld_r_d8() -> uint8_t {
+auto CPU::ld_r8_i8() -> uint8_t {
   uint8_t opcode = bus.read(reg.pc - 1);
 
   uint8_t dst = (opcode >> 3) & 0x07;
@@ -69,7 +181,111 @@ auto CPU::ld_r_d8() -> uint8_t {
   return 8;
 }
 
-auto CPU::inc_r() -> uint8_t {
+auto CPU::ld_mem_a() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+
+  uint8_t dst_addr_reg = (opcode >> 4) & 0x03;
+  uint16_t dst_addr = 0;
+
+  switch (dst_addr_reg) {
+  case 0:
+    dst_addr = reg.bc();
+    break;
+  case 1:
+    dst_addr = reg.de();
+    break;
+  case 2:
+    dst_addr = reg.hl();
+    reg.set_hl(dst_addr + 1);
+    break;
+  case 3:
+    dst_addr = reg.hl();
+    reg.set_hl(dst_addr - 1);
+    break;
+  default:
+    __builtin_unreachable();
+  }
+
+  bus.write(dst_addr, reg.a);
+
+  return 8;
+}
+
+auto CPU::ld_a_mem() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t src_addr_reg = (opcode >> 4) & 0x03;
+
+  uint16_t src_addr = 0;
+
+  switch (src_addr_reg) {
+  case 0:
+    src_addr = reg.bc();
+    break;
+  case 1:
+    src_addr = reg.de();
+    break;
+  case 2:
+    src_addr = reg.hl();
+    reg.set_hl(src_addr + 1);
+    break;
+  case 3:
+    src_addr = reg.hl();
+    reg.set_hl(src_addr - 1);
+    break;
+  default:
+    __builtin_unreachable();
+  }
+
+  reg.a = bus.read(src_addr);
+
+  return 8;
+}
+
+auto CPU::add_hl_r16() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t dst = (opcode >> 4) & 0x03;
+
+  uint16_t hl = reg.hl();
+  uint16_t val = read_r16(dst);
+
+  uint32_t result = hl + val;
+
+  // Flags
+  reg.set_flag(gb::mem::FLAG_N, false);
+  reg.set_flag(gb::mem::FLAG_H, ((hl & 0x0FFF) + (val & 0x0FFF)) > 0x0FFF);
+  reg.set_flag(gb::mem::FLAG_C, result > 0xFFFF);
+  // Z untouched
+
+  reg.set_hl(static_cast<uint16_t>(result));
+
+  return 8;
+}
+
+auto CPU::inc_r16() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t dst = (opcode >> 4) & 0x03;
+
+  uint16_t val = read_r16(dst);
+  uint16_t result = val + 1;
+
+  write_r16(dst, result);
+
+  return 8;
+}
+
+auto CPU::dec_r16() -> uint8_t {
+  uint8_t opcode = bus.read(reg.pc - 1);
+  uint8_t dst = (opcode >> 4) & 0x03;
+
+  uint16_t val = read_r16(dst);
+  uint16_t result = val - 1;
+
+  write_r16(dst, result);
+
+  return 8;
+}
+
+auto CPU::inc_r8() -> uint8_t {
   uint8_t opcode = bus.read(reg.pc - 1);
   uint8_t dst = (opcode >> 3) & 0x07;
 
@@ -87,7 +303,7 @@ auto CPU::inc_r() -> uint8_t {
   return (dst == 6) ? 12 : 4;
 }
 
-auto CPU::dec_r() -> uint8_t {
+auto CPU::dec_r8() -> uint8_t {
   uint8_t opcode = bus.read(reg.pc - 1);
   uint8_t dst = (opcode >> 3) & 0x07;
 
@@ -154,6 +370,40 @@ auto CPU::read_reg(uint8_t src) -> uint8_t {
     return bus.read(reg.hl());
   case 7:
     return reg.a;
+  default:
+    __builtin_unreachable();
+  }
+}
+
+void CPU::write_r16(uint8_t dst, uint16_t val) {
+  switch (dst) {
+  case 0:
+    reg.set_bc(val);
+    break;
+  case 1:
+    reg.set_de(val);
+    break;
+  case 2:
+    reg.set_hl(val);
+    break;
+  case 3:
+    reg.sp = val;
+    break;
+  default:
+    __builtin_unreachable();
+  }
+}
+
+auto CPU::read_r16(uint8_t src) -> uint16_t {
+  switch (src) {
+  case 0:
+    return reg.bc();
+  case 1:
+    return reg.de();
+  case 2:
+    return reg.hl();
+  case 3:
+    return reg.sp;
   default:
     __builtin_unreachable();
   }
